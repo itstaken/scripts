d0072d155331e85d31f585e94b0fd86b1bb75ae3
Stephen <stephenrd@gmail.com>
2015-10-29 08:50:19 -0400

    Updated conky config generation to include all network interfaces that are up at the time the script runs.

--- regenconf.sh
+++ regenconf.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
 ##
-# Script for regenerating my conkyconf file.
-# Generates a battery meter at the top (on my chromebooK)
-# a line displaying the wifi access point (again, on my chromebook)
-# the ip address of the wifi nic
-# a graph of network traffic on the wifi nic
+# Script for regenerating my conky config file.
+# Generates a config that contains the following:
 #
-# TODO: add wired networks
+#  * Battery percent (on my chromebooK)
+#  * Wifi ESSID (again, on my chromebook)
+#  * the ip address of the wifi NIC
+#  * a network graph
+#  * additional NICs will display ip address and graph
+#
+# TODO: add arguments
+#
+# The resulting config displays something like this:
+#
+#   Battery: 76%
+#   Wifi: attwifi
+#
+#   mlan0: up, addr 192.168.1.2
+#   Up                      Down
+#   ┌─────────────┬─────────────┐
+#   │      ╱╲     │             │
+#   │┄┄┄╱╲╱┄┄╲┄┄┄ │┄┄┄╱╲╱╲╱╲┄┄┄ │
+#   └─────────────┴─────────────┘
+#
+#   eth0: up, addr 192.168.2.2
+#   Up                      Down
+#   ┌─────────────┬─────────────┐
+#   │             │             │
+#   │┄┄┄╱╲╱╲╱╲┄┄┄┄│┄┄┄╱╲╱╲╱╲┄┄┄ │
+#   └─────────────┴─────────────┘
+#
+
 
 echo TEXT
 
 ##
 # IF the system uses battery...
-battery.sh 2>&1 >/dev/null
+battery.sh &>/dev/null
 if [ $? -eq 0 ] ; then
 cat << "EOF_battery"
 Battery: ${color green}${execi 60 battery.sh}${color white}%
@@ -21,18 +45,33 @@ EOF_battery
 fi
 
 ##
-# If the system uses wifi...
+# If the system uses wifi... and has my wifi script
 wifi=$(iwconfig 2>/dev/null | grep ESSID: | awk '{print $1}')
 if [ $? -eq 0 ] ; then
+    if [ -x wifi.sh ] ; then
 cat << EOF_wifi
 \${if_up ${wifi}}
 Wifi: \${color red}\${execi 30 wifi.sh -e}\${color white}
-${wifi}: up, addr \$alignr \${addr ${wifi}}
-Up \${alignr} Down
-\${upspeed ${wifi}}\${alignr}\${downspeed ${wifi}}
-\${upspeedgraph ${wifi} 50,100 006600 00ff00}\${alignr}\${downspeedgraph ${wifi} 50,100 006600 00ff00}
-\${else}
-${wifi}: down
 \${endif}
 EOF_wifi
+    fi
 fi
+
+##
+# Enumerate all devices and create an etnry for them...
+while read -r -d $'\n' ETHER; do
+ifconfig "${ETHER}" | grep 'inet addr' &> /dev/null
+if [ "$?" -eq 0 ] ; then
+cat << EOF_eth
+\${if_up ${ETHER}}
+${ETHER}: up, addr \$alignr \${addr ${ETHER}}
+Up \${alignr} Down
+\${upspeed ${ETHER}}\${alignr}\${downspeed ${ETHER}}
+\${upspeedgraph ${ETHER} 50,100 006600 00ff00}\${alignr}\${downspeedgraph ${ETHER} 50,100 006600 00ff00}
+\${else}
+${ETHER}: down
+\${endif}
+EOF_eth
+fi
+
+done < <(ifconfig -a | grep Ethernet | awk '{print $1}')
