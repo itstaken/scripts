bb843a8e045ab3927a368bcd66610194bfdef7b4
itstaken <itstaken@>
2025-02-17 22:58:30 -0500

    Updated battery script to display more information and updated conky's use of thew battery info.

--- battery.sh
+++ battery.sh
@@ -11,10 +11,34 @@
 POWER_SUPPLY_CLASS=/sys/class/power_supply/
 HAPPY=1
 if [ -d "${POWER_SUPPLY_CLASS}" ] ; then
+#    COUNT=$(ls "${POWER_SUPPLY_CLASS}" | wc -l)
     for f in "${POWER_SUPPLY_CLASS}"/* ; do
+        TYPE_PATH=${f}/type
         CAPACITY_PATH=${f}/capacity
+        NAME_PATH=${f}/device/name
+        STATUS_PATH=${f}/status
+        HEALTH_PATH=${f}/health
+        PRESENT_PATH=${f}/present
+        if [ -f "${PRESENT_PATH}" ] ; then
+            PRESENT=$(cat "${PRESENT_PATH}")
+        else
+            PRESENT=0
+        fi
         if [ -f "${CAPACITY_PATH}" ] ; then
-            cat "${CAPACITY_PATH}"
+            CAPACITY=$(cat "${CAPACITY_PATH}")
+            TYPE=$(cat "${TYPE_PATH}")
+            NAME=$(cat "${NAME_PATH}")
+            STATUS=$(cat "${STATUS_PATH}")
+            HEALTH=$(cat "${HEALTH_PATH}")
+            if [ "${HEALTH}" != "Good" ] ; then
+                echo "[${NAME}] ${CAPACITY}% (${HEALTH})"
+            else
+                if [ "${PRESENT}" -ne 1 ] ; then
+                    echo "[${NAME}] Missing"
+                else
+                    echo "[${NAME}] ${CAPACITY}% (${STATUS})"
+                fi
+            fi
             HAPPY=0
         fi
     done
